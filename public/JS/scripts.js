e9f755dc8aadd1ec5962c5ba975cc51e361603cc
Matias <matiasinsaurralde51@gmail.com>
2021-05-21 14:40:06 -0300

    msj funcionando

--- public/JS/scripts.js
+++ public/JS/scripts.js
@@ -26,3 +26,63 @@ function DownLoadCV() {
 }
 
 DownLoadCV();
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBHxXM3egKldQVjZ4xuI9zTafRsiZux9Bk",
+  authDomain: "probandofirebase-a9522.firebaseapp.com",
+  projectId: "probandofirebase-a9522",
+  storageBucket: "probandofirebase-a9522.appspot.com",
+  messagingSenderId: "919979696763",
+  appId: "1:919979696763:web:02441f407cbfc66e0938de",
+  measurementId: "G-GV7D0P59P4",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var messagesRef = firebase.database().ref("messages");
+
+// Formulario CORREO
+
+document.getElementById("contacForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  // Obtener valores
+
+  var name = getInputVal("name");
+  var email = getInputVal("email");
+  var message = getInputVal("message");
+
+  // Guardando mensaje
+  saveMessage(name, email, message);
+
+  // Mostrar Alerta
+
+  document.querySelector(".alertxd").style.display = "block";
+
+  // Ocultar alerta
+
+  setTimeout(function () {
+    document.querySelector(".alertxd").style.display = "none";
+  }, 3000);
+}
+
+// Funcion para obtener el valor de los forms
+
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
+
+// Guardar mensaje
+
+function saveMessage(name, email, message) {
+  var newMessageRef = messagesRef.push();
+
+  newMessageRef.set({
+    name: name,
+    email: email,
+    message: message,
+  });
+}
